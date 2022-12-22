b480a179ea451b22f65260508c3a3c2d12fc2c6c
Alex <eddings.sharanna@gmail.com>
2022-12-22 13:40:37 -0500

    added Auth page

---
+++ src/pages/AuthPage/AuthPage.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import styles from "../AuthPage/AuthPage.module.css";
+import LoginForm from "../../components/LoginForm";
+import SignUpForm from "../../components/SignUpForm";
+
+export default function AuthPage({ setUser }) {
+  const [showLogin, setShowLogin] = useState(true);
+
+  return (
+    <main className={styles.AuthPage}>
+      <div>
+        <h3 onClick={() => setShowLogin(!showLogin)}>
+          {showLogin ? "SIGN UP" : "LOG IN"}
+        </h3>
+      </div>
+      {showLogin ? (
+        <LoginForm setUser={setUser} />
+      ) : (
+        <SignUpForm setUser={setUser} />
+      )}
+    </main>
+  );
+}
