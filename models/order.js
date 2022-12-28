239b9f6d69839b0fada1b2b3da63b6cf497084d0
Alex <eddings.sharanna@gmail.com>
2022-12-28 10:34:37 -0500

    updates too all files

--- models/order.js
+++ models/order.js
@@ -20,23 +20,14 @@ lineItemSchema.virtual("extPrice").get(function () {
 
 const orderSchema = new Schema(
   {
-    userId: { type: String, required: true },
-    products: [
-      {
-        productId: {
-          type: String,
-        },
-        quantity: {
-          type: Number,
-          default: 1,
-        },
-      },
-    ],
-    amount: { type: Number, required: true },
-    address: { type: Object, required: true },
-    status: { type: String, default: "pending" },
+    user: { type: Schema.Types.ObjectId, ref: "User" },
+    lineItems: [lineItemSchema],
+    isPaid: { type: Boolean, default: false },
   },
-  { timestamps: true }
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+  }
 );
 
 orderSchema.virtual("orderTotal").get(function () {
